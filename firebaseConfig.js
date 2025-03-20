c9cf25e9a984140e6bc4c245866884db871cc16d
ranielletuazon <radtuazon.student@ua.edu.ph>
2025-03-20 14:57:52 +0800

    fix

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-
 const admin = require("firebase-admin");
-const serviceAccount = require("./orbit-5c69d-firebase-adminsdk-imdm8-f72fdfdbcc.json");
+
+const serviceAccountJson = Buffer.from(process.env.FIREBASE_ADMINSDK, "base64").toString("utf8");
+const serviceAccount = JSON.parse(serviceAccountJson);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
